f4dd4602aabcd6048a1d0fee270b3e9cc91dbae6
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-04-17 18:54:14 +0300

    ft:App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,23 @@
 import { useState } from 'react'
-
-
-
+import Products from './components/products'
+import {Route,Routes} from 'react-router-dom'
+import Home from './components/Home'
+import AddProducts from './components/AddProducts'
+import ProductDetails from './components/ProductDetails'
+import Cart from './components/Cart'
 function App() {
   
 
   return (
     <>
-     <h1 className='text-center text-3xl font-bold'>Group C members</h1>
-     <ul className='text-center text-2xl'>
-      <li>Simon Mwangi</li>
-      <li>Plip Wekullo</li>
-      <li>Pricilla Wakahia</li>
-      <li>Victor Muteithia</li>
-     </ul>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/products" element={<Products />} />
+      <Route path="/AddProducts" element={<AddProducts />} />
+      <Route path="/Details/:id" element={<ProductDetails />} />
+      <Route path="/cart" element={<Cart />} />
+    </Routes>
+     
     </>
   )
 }
