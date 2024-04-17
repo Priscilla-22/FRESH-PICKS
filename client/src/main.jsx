3d192e8fe42d3c6b059df97ca75c3f7a1c122b0f
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-04-17 18:50:26 +0300

    ft:main

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import 'react-toastify/dist/ReactToastify.css';
+import { BrowserRouter } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+  <BrowserRouter>
+     <App />
     <ToastContainer/>
+  </BrowserRouter>
+   
   </React.StrictMode>,
 )
