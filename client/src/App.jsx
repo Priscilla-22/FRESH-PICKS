15a79c152bbcab14c23530e45dd9b0058aa046f6
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-04-17 18:54:14 +0300

    ft:App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,33 @@
 import { useState } from 'react'
 import Products from './components/products'
 import {Route,Routes} from 'react-router-dom'
+import Homepage from './Components/HomePage/HomePage.jsx';
 
 import AddProducts from './components/AddProducts'
 import ProductDetails from './components/ProductDetails'
 import Cart from './components/Cart'
-import Checkout from './components/Checkout'
-import Homepage from './Components/HomePage/HomePage.jsx';
-function App() {
+
   
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Homepage from './Components/HomePage/HomePage.jsx';
+import './index.css';
 
-
-
-
-
+function App() {
   return (
-    <>
+    <div className='App'>
     <Routes>
-      <Route path="/" element={<Homepage />} />
+      <Route path='/' element={<Homepage />} />
       <Route path="/products" element={<Products />} />
       <Route path="/AddProducts" element={<AddProducts />} />
       <Route path="/Details/:id" element={<ProductDetails />} />
-      <Route path="/checkout/" element={<Checkout />} />
       <Route path="/cart" element={<Cart />} />
     </Routes>
      
-    </>
+    </div>
   )
 }
+    
+
 
 export default App;
