53b53f3dc3568d1b4bc2cd93484f531b0eb0caa8
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-04-18 16:52:50 +0300

    merging

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,31 @@
 
+import Homepage from './Components/HomePage/HomePage.jsx';
+import './index.css';
+
+import { useState } from 'react'
 import Products from './components/products'
 import {Route,Routes} from 'react-router-dom'
-import Homepage from './Components/HomePage/HomePage.jsx';
-
+import Home from './components/Home'
 import AddProducts from './components/AddProducts'
 import ProductDetails from './components/ProductDetails'
 import Cart from './components/Cart'
-import Checkout from './components/Checkout'
-import Homepage from './Components/HomePage/HomePage.jsx';
 function App() {
+
+
   return (
-    <div className='App'>
+   
+    <>
     <Routes>
-      <Route path="/" element={<Homepage />} />
+    <Route path='/' element={<Homepage />} />
+     
       <Route path="/products" element={<Products />} />
       <Route path="/AddProducts" element={<AddProducts />} />
       <Route path="/Details/:id" element={<ProductDetails />} />
-      <Route path="/checkout/" element={<Checkout />} />
       <Route path="/cart" element={<Cart />} />
     </Routes>
      
-    </div>
+    </>
   )
 }
-    
-
 
 export default App;
