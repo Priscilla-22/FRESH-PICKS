d1cdc2694eca24af6e8b583fd604548ba66b2096
victormuteithia <muteithiavictor@gmail.com>
2024-04-18 05:18:23 +0300

    Branch model/api created

--- server/app.py
+++ server/app.py
@@ -1,5 +1,5 @@
 from flask import Flask,request,make_response
-from models import db
+from models import db, Branch
 from flask_migrate import Migrate
 from flask_restful import Api,Resource
 import os
@@ -16,6 +16,66 @@ db.init_app(app)
 @app.route('/')
 def home():
     return "<h1>Welcome to group C api</h1>"
+
+class Branch(Resource):
+    def get(self):
+        response_dict_list = [branch.to_dict for branch in Branch.query.all()]
+
+        response = make_response(response_dict_list, 200)
+        return response
+    
+    def post(self):
+        new_branch = Branch(
+            name = request.form['name'],
+            location = request.form['location']
+        )
+
+        db.session.add(new_branch)
+        db.session.commit()
+
+        response_dict = new_branch.to_dict()
+        response = make_response(response_dict, 201)
+
+        return response
+
+api.add_resource(Branch, '/branches')
+
+
+class BranchByID(Resource):
+    def get(self, id):
+        response_dict = Branch.query.filter(Branch.id == id).first().to_dict()
+
+        response = make_response(response_dict, 200)
+        return response
+    
+    def patch(self, id):
+        branch = Branch.query.filter(Branch.id == id).first()
+
+        for attr in request.form:
+            setattr(branch, attr, request.form[attr])
+
+        db.session.add(branch)
+        db.session.commit()
+
+        response = make_response(branch.to_dict, 200)
+        return response
+    
+    def delete(self, id):
+        branch = Branch.query.filter(Branch.id == id).first()
+
+        db.session.delete(branch)
+        db.session.commit()
+
+        response = make_response(
+            {"message": f"Branch {id} was closed"},
+            200
+        )
+        return response
+    
+api.add_resource(BranchByID, '/branches/<int:id>')
+
+
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -8,3 +8,27 @@ metadata = MetaData(naming_convention={
 })
 
 db = SQLAlchemy(metadata=metadata)
+
+class Branch(db.Model, SerializerMixin):
+    __tablename__ = 'branches'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    location = db.Column(db.String, nullable=False)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+
+    @validates('name')
+    def validate_name(self, key, name):
+        ex_branch = Branch.query.filter(Branch.name == name).first()
+        if ex_branch:
+            raise NameError("Name already exists")
+        return name
+    @validates('location')
+    def validate_location(self, key, location):
+        ex_location = Branch.query.filter(Branch.location == location).first()
+        if ex_location:
+            raise NameError("Location already exists")
+        return location
+
+    def __repr__(self):
+        return f'Branch {self.id}, {self.name}, {self.location}, {self.created_at}'
