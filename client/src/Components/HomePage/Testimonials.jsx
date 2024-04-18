cc6118b8a11d4853e7191a5727a55052ea2c58cb
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-04-18 17:45:18 +0300

    Ft:merging

--- client/src/Components/HomePage/Testimonials.jsx
+++ client/src/Components/HomePage/Testimonials.jsx
@@ -8,7 +8,7 @@ const Testimonials = () => {
   const testimonials = [
     {
       id: 1,
-<<<<<<< HEAD
+
       name: 'Simon Mwangi',
       title: 'CEO of Acme Inc.',
       quote:
@@ -34,7 +34,7 @@ const Testimonials = () => {
       title: 'Marketing Manager',
       quote:
         'I love how easy it is to use Fresh Picks. The website is intuitive and the selection of products is impressive.',
-=======
+
       name: 'John Doe',
       title: 'CEO of Acme Inc.',
       quote: 'Fresh Picks has completely changed the way I do grocery shopping. The produce is always fresh and the delivery is fast and reliable.'
@@ -50,7 +50,7 @@ const Testimonials = () => {
       name: 'Mike Johnson',
       title: 'Software Engineer',
       quote: 'I was skeptical at first, but Fresh Picks has won me over. The quality of the food is excellent and the convenience can\'t be beat.'
->>>>>>> 24d4364 (feat: initial homepage  structure)
+
     }
   ];
 
@@ -66,15 +66,9 @@ const Testimonials = () => {
   };
 
   return (
-<<<<<<< HEAD
     <div className="py-10 bg-gray-100">
       <div className="container mx-auto text-center">
         <h2 className="text-4xl font-bold mb-5">What Our Customers Are Saying</h2>
-=======
-    <div className="py-16 bg-gray-100">
-      <div className="container mx-auto text-center">
-        <h2 className="text-4xl font-bold mb-8">What Our Customers Are Saying</h2>
->>>>>>> 24d4364 (feat: initial homepage  structure)
         <Slider {...settings}>
           {testimonials.map((testimonial) => (
             <div key={testimonial.id} className="text-center">
