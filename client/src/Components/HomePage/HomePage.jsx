8f9da035761341f3a06574b808a3e6e6ca531b2d
Priscilla-22 <priscillamw123@gmail.com>
2024-04-18 12:34:37 +0300

    chore: fix

--- client/src/Components/HomePage/HomePage.jsx
+++ client/src/Components/HomePage/HomePage.jsx
@@ -3,6 +3,10 @@ import Hero from './Hero';
 import Featured from './Featured';
 import Categories from './Categories';
 import Testimonials from './Testimonials';
+<<<<<<< HEAD
+=======
+// import CallToAction from './CallToAction';
+>>>>>>> 1d09b12 (chore: fix)
 import Footer from './Footer';
 
 function HomePage() {
@@ -13,6 +17,10 @@ function HomePage() {
       <Featured />
       <Categories />
       <Testimonials />
+<<<<<<< HEAD
+=======
+      {/* <CallToAction /> */}
+>>>>>>> 1d09b12 (chore: fix)
       <Footer />
     </div>
   );
