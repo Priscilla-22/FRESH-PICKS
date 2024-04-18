63dad4356348e462f1179a2888732592d3291850
Priscilla-22 <priscillamw123@gmail.com>
2024-04-19 00:43:52 +0300

    fix

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import './index.css';
 import 'react-toastify/dist/ReactToastify.css';
-import { BrowserRouter } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-  <BrowserRouter>
-     <App />
-    <ToastContainer/>
-  </BrowserRouter>
-   
-  </React.StrictMode>,
-)
+    <App />
+    <ToastContainer />
+  </React.StrictMode>
+);
