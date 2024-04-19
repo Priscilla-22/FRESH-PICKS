dfb7f853f2a6de8a4f88205072adab06e6373295
victormuteithia <muteithiavictor@gmail.com>
2024-04-19 08:18:45 +0300

    amended app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,9 @@
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Products from './components/products'
+
 
 import Login from "./pages/Login"
 import Signup from "./pages/Signup"
-import AddProducts from './components/AddProducts'
-import ProductDetails from './components/ProductDetails'
-import Cart from './components/Cart'
-import Checkout from './components/Checkout'
-import Homepage from './Components/HomePage/HomePage.jsx';
-
 
 import './index.css';
 
@@ -36,24 +30,11 @@ function App() {
           <Route path="/AddProducts" element={<AddProducts />} />
           <Route path="/Details/:id" element={<ProductDetails />} />
           <Route path="/cart" element={<Cart />} />        
+          <Route path="/branches" element={<Branches/>}/>
         </Routes>      
       </main>
     </Router>
-   
-   
-    <>
-    <Routes>
-      <Route path="/" element={<Homepage />} />
-      <Route path="/products" element={<Products />} />
-      <Route path="/checkout" element={<Checkout />} />
-      <Route path="/AddProducts" element={<AddProducts />} />
-      <Route path="/Details/:id" element={<ProductDetails />} />
-      <Route path="/cart" element={<Cart />} />
-      <Route path="/branches" element={<Branches/>}/>
-      
-    </Routes>
-     
-    </>
+
   )
 }
 
