e5405a2886145c26a68bbdd44d8f0dc791bdca00
Priscilla-22 <priscillamw123@gmail.com>
2024-04-21 21:13:21 +0300

    feat:cart

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,21 +1,20 @@
 import React from 'react';
-import { createRoot } from 'react-dom'; 
+import { createRoot } from 'react-dom';
 import App from './App.jsx';
 import './index.css';
 import store from './features/store.jsx';
-import { Provider } from'react-redux';
+import { Provider } from 'react-redux';
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
 import { BrowserRouter } from 'react-router-dom';
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-  <Provider store={store}>
-     <BrowserRouter>
-    <App />
-    <ToastContainer />
-  </BrowserRouter>
-  </Provider>
- 
-    
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+        <ToastContainer />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
