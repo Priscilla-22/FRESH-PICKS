214db609ff86bb4ab01e9f96a1889143208480f6
Priscilla-22 <priscillamw123@gmail.com>
2024-04-21 13:05:34 +0300

    feat: Update React code to use createRoot in React 18

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom'; 
 import App from './App.jsx';
-import './index.css'
+import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
 
-ReactDOM.render(
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-        <App />
-   </BrowserRouter>
-
+      <App />
+    </BrowserRouter>
     <ToastContainer />
   </React.StrictMode>
 );
