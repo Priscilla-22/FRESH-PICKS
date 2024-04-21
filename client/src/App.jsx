941985301aa0e442dcbca1f4e3ae87fbdf10ef75
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-04-21 16:42:09 +0300

    Cart changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,7 +17,6 @@ function App() {
           <Route path="/" element={<Homepage />} />
           <Route path='/login' element={<Login/>}/>
           <Route path='/signup' element={<Signup/>}/>
-          <Route path="/" element={<Homepage />} />
           <Route path="/products" element={<Products />} />
           <Route path="/checkout" element={<Checkout />} />
           <Route path="/AddProducts" element={<AddProducts />} />
